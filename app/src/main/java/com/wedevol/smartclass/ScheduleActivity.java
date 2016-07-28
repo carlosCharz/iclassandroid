403b3de61e263b266e751f5beae4a442cc788f12
Carlos Becerra <carlos.becerra1234@gmail.com>
2016-07-28 16:58:34 +0200

    Refactoring: adding the time picker in the edit text for the schedule activity

--- app/src/main/java/com/wedevol/smartclass/ScheduleActivity.java
+++ app/src/main/java/com/wedevol/smartclass/ScheduleActivity.java
@@ -1,11 +1,14 @@
 package com.wedevol.smartclass;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TimePicker;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -16,6 +19,8 @@ public class ScheduleActivity extends AppCompatActivity {
     Button _completeViewButton;
     @BindView(R.id.btn_save_schedule)
     Button _saveScheduleButton;
+    @BindView(R.id.from1Text)
+    EditText _from1Text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +45,20 @@ public class ScheduleActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        _from1Text.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                TimePickerDialog timePickerDialog = new TimePickerDialog(ScheduleActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        _from1Text.setText(hourOfDay + ":" + minute);
+                    }
+                }, 0, 0, true);
+                timePickerDialog.show();
             }
         });
     }
