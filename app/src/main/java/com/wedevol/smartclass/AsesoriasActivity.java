8ef4d2a0c5ec27a4b6023c7113feb6f25f6d5cf7
Carlos Becerra <carlos.becerra1234@gmail.com>
2016-08-17 20:00:49 +0200

    Finish asesoriasDetail activity

--- app/src/main/java/com/wedevol/smartclass/AsesoriasActivity.java
+++ app/src/main/java/com/wedevol/smartclass/AsesoriasActivity.java
@@ -1,21 +1,39 @@
 package com.wedevol.smartclass;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class AsesoriasActivity extends AppCompatActivity {
+
+    @BindView(R.id.asesoriaRow)
+    LinearLayout asesoriaRow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_asesorias);
+        ButterKnife.bind(this);
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Asesorías");
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        asesoriaRow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AsesoriasDetailActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
