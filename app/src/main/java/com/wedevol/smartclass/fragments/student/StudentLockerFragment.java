45822251fe733405a7b5e693989d8220929c4acd
Paolo Rossi <paolorossi@Paolos-Mac-mini.local>
2016-12-09 17:40:08 -0500

    added counseling

--- app/src/main/java/com/wedevol/smartclass/fragments/student/StudentLockerFragment.java
+++ app/src/main/java/com/wedevol/smartclass/fragments/student/StudentLockerFragment.java
@@ -7,6 +7,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.wedevol.smartclass.R;
@@ -15,6 +17,8 @@ import com.wedevol.smartclass.models.Class;
 import com.wedevol.smartclass.models.Course;
 import com.wedevol.smartclass.models.Instructor;
 import com.wedevol.smartclass.models.Schedule;
+import com.wedevol.smartclass.utils.UtilMethods;
+import com.wedevol.smartclass.utils.interfaces.Constants;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -22,9 +26,6 @@ import java.util.List;
 
 /** Created by paolorossi on 12/9/16.*/
 public class StudentLockerFragment extends Fragment{
-    private TextView tv_no_counsels;
-    private RecyclerView rv_pending_counsels;
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,24 +41,37 @@ public class StudentLockerFragment extends Fragment{
     }
 
     private void setElements(View view) {
+        ImageView iv_user_profile_photo = (ImageView) view.findViewById(R.id.iv_user_profile_photo);
+        TextView tv_student_level = (TextView) view.findViewById(R.id.tv_student_level);
+        ProgressBar pb_student_progress = (ProgressBar) view.findViewById(R.id.pb_student_progress);
+        TextView tv_student_counselled_time = (TextView) view.findViewById(R.id.tv_student_counselled_time);
+
+        UtilMethods.setPhoto(getActivity(), iv_user_profile_photo, "", Constants.USER_PHOTO);
+        //student.getClasses().size() + " cursos"
+        tv_student_level.setText("20 cursos");
+        //(int) student.getRating()
+        pb_student_progress.setProgress(45);
+        //student.getTotalHours() + " hrs"
+        tv_student_counselled_time.setText("50 hrs");
+
         List<Class> pendingCounsels = new ArrayList<>();
         pendingCounsels.add(new Class(1, new Instructor(), new Course(), new Schedule(), new Date(), "status"));
         pendingCounsels.add(new Class(2, new Instructor(), new Course(), new Schedule(), new Date(), "status"));
         pendingCounsels.add(new Class(3, new Instructor(), new Course(), new Schedule(), new Date(), "status"));
 
-        tv_no_counsels = (TextView) view.findViewById(R.id.tv_no_counsels);
+        TextView tv_no_counsels = (TextView) view.findViewById(R.id.tv_no_counsels);
 
-        rv_pending_counsels = (RecyclerView) view.findViewById(R.id.rv_pending_counsels);
+        RecyclerView rv_pending_counsels = (RecyclerView) view.findViewById(R.id.rv_pending_counsels);
         rv_pending_counsels.setLayoutManager(new LinearLayoutManager(getActivity()));
         rv_pending_counsels.setAdapter(new PendingCounselsAdapter(getActivity(), pendingCounsels));
 
         if(pendingCounsels.size() == 0){
-
             tv_no_counsels.setVisibility(View.VISIBLE);
             rv_pending_counsels.setVisibility(View.GONE);
         }
     }
 
     private void setActions() {
+
     }
 }
