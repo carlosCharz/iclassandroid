6ac9a7d5ec68fdf3dbc56da4bc1ff2611b4ed9ef
antaresr <diagonal_zero@hotmail.com>
2017-01-13 18:10:02 -0500

    loader on courses

--- app/src/main/java/com/wedevol/smartclass/fragments/instructor/InstructorCoursesFragment.java
+++ app/src/main/java/com/wedevol/smartclass/fragments/instructor/InstructorCoursesFragment.java
@@ -10,12 +10,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ProgressBar;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.wedevol.smartclass.R;
 import com.wedevol.smartclass.activities.HomeActivity;
 import com.wedevol.smartclass.activities.instructor.EnableCourseActivity;
+import com.wedevol.smartclass.adapters.ListBanksAdapter;
 import com.wedevol.smartclass.adapters.ListCourseStateAdapter;
 import com.wedevol.smartclass.models.Course;
 import com.wedevol.smartclass.utils.interfaces.Constants;
@@ -69,6 +71,7 @@ public class InstructorCoursesFragment extends Fragment{
     }
 
     private void getCourses(final View view) {
+        final ProgressBar pb_charging = (ProgressBar) view.findViewById(R.id.pb_charging);
         int id = ((HomeActivity)getActivity()).getmPreferencesManager().getUserInfo().getId();
         RestClient restClient = new RestClient(getActivity());
 
@@ -85,8 +88,16 @@ public class InstructorCoursesFragment extends Fragment{
                 }
 
                 RecyclerView rv_payed = (RecyclerView) view.findViewById(R.id.rv_payed);
-                rv_payed.setLayoutManager(new LinearLayoutManager(getActivity()));
-                rv_payed.setAdapter(new ListCourseStateAdapter(getActivity(), courseList, "PAGADO", "Eres un asesor de este curso", Constants.SHOW_COURSE_PRICE, Constants.NOT_SELECTABLE_COURSE));
+
+                if(courseList.size() == 0 ){
+                    rv_payed.setVisibility(View.GONE);
+                }else{
+                    rv_payed.setLayoutManager(new LinearLayoutManager(getActivity()));
+                    rv_payed.setAdapter(new ListCourseStateAdapter(getActivity(), courseList, "PAGADO", "Eres un asesor de este curso", Constants.SHOW_COURSE_PRICE, Constants.NOT_SELECTABLE_COURSE));
+                    rv_payed.setVisibility(View.VISIBLE);
+                }
+
+                pb_charging.setVisibility(View.GONE);
                 /* RecyclerView rv_verify_payment = (RecyclerView) view.findViewById(R.id.rv_verify_payment);
                 rv_verify_payment.setLayoutManager(new LinearLayoutManager(getActivity()));
                 rv_verify_payment.setAdapter(new ListCourseStateAdapter(getActivity(), pairList, "VERIFICANDO PAGO", "Maximo 24 horas", Constants.SHOW_COURSE_PRICE, Constants.NOT_SELECTABLE_COURSE));
