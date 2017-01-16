f7d600e1f09d597ad318ca143baeadc0823ae2ca
antaresr <diagonal_zero@hotmail.com>
2017-01-16 17:54:29 -0500

    added requested

--- app/src/main/java/com/wedevol/smartclass/fragments/student/StudentRequestFragment.java
+++ app/src/main/java/com/wedevol/smartclass/fragments/student/StudentRequestFragment.java
@@ -7,14 +7,24 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.wedevol.smartclass.R;
 import com.wedevol.smartclass.adapters.ListLessonsAdapter;
 import com.wedevol.smartclass.models.Lesson;
+import com.wedevol.smartclass.models.Student;
+import com.wedevol.smartclass.utils.SharedPreferencesManager;
 import com.wedevol.smartclass.utils.interfaces.Constants;
+import com.wedevol.smartclass.utils.retrofit.IClassCallback;
+import com.wedevol.smartclass.utils.retrofit.RestClient;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import retrofit.client.Response;
 
 /** Created by paolorossi on 12/8/16.*/
 public class StudentRequestFragment extends Fragment{
@@ -33,12 +43,37 @@ public class StudentRequestFragment extends Fragment{
         return view;
     }
 
-    private void setElements(View view){
-        List<Lesson> undefinedClass = new ArrayList<>();
+    private void setElements(final View view){
+        RestClient restClient = new RestClient(getActivity());
+        Student student = (Student) SharedPreferencesManager.getInstance(getActivity()).getUserInfo();
 
-        RecyclerView rv_student_requests = (RecyclerView) view.findViewById(R.id.rv_student_requests);
-        rv_student_requests.setLayoutManager(new LinearLayoutManager(getActivity()));
-        rv_student_requests.setAdapter(new ListLessonsAdapter(getActivity(), undefinedClass, Constants.REQUEST_TYPE));
+        final List<Lesson> requestedlessonList = new ArrayList<>();
+        final ProgressBar pb_charging = (ProgressBar) view.findViewById(R.id.pb_charging);
+
+        restClient.getWebservices().studentLessons("", student.getId(), "8/1/2017", 2, "requested", new IClassCallback<JsonArray>(getActivity()) {
+            @Override
+            public void success(JsonArray jsonArray, Response response) {
+                super.success(jsonArray, response);
+                for(JsonElement jsonElement: jsonArray){
+                    requestedlessonList.add(Lesson.parseLesson(jsonElement.getAsJsonObject()));
+                }
+
+                RecyclerView rv_student_requests = (RecyclerView) view.findViewById(R.id.rv_student_requests);
+                rv_student_requests.setLayoutManager(new LinearLayoutManager(getActivity()));
+                rv_student_requests.setAdapter(new ListLessonsAdapter(getActivity(), requestedlessonList, Constants.REQUEST_TYPE));
+
+                if(requestedlessonList.size() == 0){
+                    TextView tv_no_counselings = (TextView) view.findViewById(R.id.tv_no_counselings);
+                    tv_no_counselings.setVisibility(View.VISIBLE);
+                    rv_student_requests.setVisibility(View.GONE);
+                }
+
+                pb_charging.setVisibility(View.GONE);
+            }
+        });
+
+
+
     }
 
     private void setActions() {
